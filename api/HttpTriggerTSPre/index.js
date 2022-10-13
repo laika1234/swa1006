9769b3e3db2f7e3f355c59591826fdd46a747e7f
Tom Luo <v-luhui@microsoft.com>
2022-10-13 18:07:59 +0800

    test pre

--- api/HttpTriggerTSPre/index.js
+++ api/HttpTriggerTSPre/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('this is pre-env');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello, " + name + ". this is pre-env."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
