2f3544ab3a58c5e7adbde2487849666a24e575cd
Tom Luo <v-luhui@microsoft.com>
2022-10-13 22:19:04 +0800

    add app setting

--- api/HttpTriggerTSPre/index.js
+++ api/HttpTriggerTSPre/index.js
@@ -1,10 +1,8 @@
 module.exports = async function (context, req) {
     context.log('this is pre-env');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". this is pre-env."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const Authorization__AzureAD__ClientId = process.env.Authorization__AzureAD__ClientId;
+    const responseMessage = "Authorization__AzureAD__ClientId:"+Authorization__AzureAD__ClientId;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
